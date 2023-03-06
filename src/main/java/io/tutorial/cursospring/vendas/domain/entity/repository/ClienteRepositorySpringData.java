66ada77fa73dd502c3c540cc1211a5b6488923eb
euFilpeSilva <sousadasilvafilipe@gmail.com>
2023-03-05 21:49:38 -0300

    testando Query Methods

--- src/main/java/io/tutorial/cursospring/vendas/domain/entity/repository/ClienteRepositorySpringData.java
+++ src/main/java/io/tutorial/cursospring/vendas/domain/entity/repository/ClienteRepositorySpringData.java
@@ -1,6 +1,7 @@
 package io.tutorial.cursospring.vendas.domain.entity.repository;
 
 import io.tutorial.cursospring.vendas.domain.entity.Cliente;
+import org.h2.command.dml.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,14 @@ import java.util.List;
 @Repository
 public interface ClienteRepositorySpringData extends JpaRepository<Cliente, Integer> {
 
+//    O QueryMethod a abaixo equivale a seguinte consulta SQL:
+//    SELECT c FROM Cliente c WHERE c.nome LIKE :nome
     List<Cliente> findByNomeLike(String cli);
+
+//    mais um exemplo abaixo:
+    List<Cliente> findByNomeOrIdOrderById(String nome, Integer id);
+
+//    outro
+    Cliente findOneByNome(String nome);
 }
 
