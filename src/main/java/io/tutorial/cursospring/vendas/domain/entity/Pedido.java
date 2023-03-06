9eac4610906feaa7ee54312c2a27b897d6d860e3
euFilpeSilva <sousadasilvafilipe@gmail.com>
2023-03-05 22:21:55 -0300

    criando relacionamentos e mapeando entitys/classes com JPA

--- src/main/java/io/tutorial/cursospring/vendas/domain/entity/Pedido.java
+++ src/main/java/io/tutorial/cursospring/vendas/domain/entity/Pedido.java
@@ -1,10 +1,27 @@
 package io.tutorial.cursospring.vendas.domain.entity;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
 
+@Entity
+@Table(name = "produto")
 public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
+
+    @Column(name = "data_pedido")
+    private LocalDate dataPedido;
+
+    @Column(name = "total", length = 20, precision = 2)
     private BigDecimal total;
 
     public Integer getId() {
