feb9442f0de42cc5b7f6191b9893fdfc8281434c
euFilpeSilva <sousadasilvafilipe@gmail.com>
2023-03-05 21:19:27 -0300

    Subindo codigo

---
+++ src/main/java/io/tutorial/cursospring/vendas/domain/entity/Pedido.java
@@ -0,0 +1,28 @@
+package io.tutorial.cursospring.vendas.domain.entity;
+
+import java.math.BigDecimal;
+
+public class Pedido {
+    private Integer id;
+    private Cliente cliente;
+    private BigDecimal total;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    public BigDecimal getTotal() {
+        return total;
+    }
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+}
---
+++ src/main/java/io/tutorial/cursospring/vendas/services/ClienteServices.java
@@ -0,0 +1,10 @@
+package io.tutorial.cursospring.vendas.services;
+
+import io.tutorial.cursospring.vendas.domain.entity.repository.ClientesRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServices {
+
+    private ClientesRepository repository;
+}
